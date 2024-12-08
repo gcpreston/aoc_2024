d65d78982a5c80ed252e17a59e887f2e3da70e39
Graham Preston <graham.preston@gmail.com>
2024-12-08 22:27:11 +0100

    Day 1 part 2

--- lib/solutions/day_01.ex
+++ lib/solutions/day_01.ex
@@ -1,7 +1,7 @@
 defmodule Aoc2024.Day01 do
   @day_number 1
 
-  def part1 do
+  defp input_left_and_right do
     input_lines =
       Aoc2024.input_for(@day_number)
       |> String.trim()
@@ -12,8 +12,27 @@ defmodule Aoc2024.Day01 do
     left = Enum.map(input_split, fn n -> String.to_integer(List.first(n)) end) |> Enum.sort()
     right = Enum.map(input_split, fn n -> String.to_integer(List.last(n)) end) |> Enum.sort()
 
+    {left, right}
+  end
+
+  def part1 do
+    {left, right} = input_left_and_right()
+
     Enum.zip(left, right)
     |> Enum.map(fn {l, r} -> abs(l - r) end)
+    |> Enum.sum()
+  end
+
+  def part2 do
+    {left, right} = input_left_and_right()
+
+    occurrences = Enum.reduce(right, %{}, fn r, acc ->
+      Map.update(acc, r, 1, fn r_old -> r_old + 1 end)
+    end)
+
+    left
+    |> Enum.map(fn l -> Map.get(occurrences, l, 0) * l end)
+    |> Enum.filter(fn l -> l != 0 end)
     |> Enum.sum()
   end
 end
