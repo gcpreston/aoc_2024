f8a240d4c2ed89e56bf45fd0cb2964bbff72fcd6
Graham Preston <graham.preston@gmail.com>
2025-01-12 02:37:29 +0100

    Day 6 part 2 except it's too slow lol

--- lib/solutions/day_06.ex
+++ lib/solutions/day_06.ex
@@ -1,4 +1,51 @@
 defmodule Aoc2024.Day06 do
+  ## Part 2
+
+  # IDEA
+  # - Brute force solution: wishlist
+  #   * loops?(map, position, direction) : indicates if a map + starting point + starting direction causes the guard to loop
+
+  defp looping_obstacle_positions(map, pos, dir, rc, cc), do: looping_obstacle_positions(map, pos, dir, rc, cc, {0, 0}, [])
+  defp looping_obstacle_positions(_map, _pos, _dir, _rc, _cc, nil, found_loops), do: found_loops
+
+  defp looping_obstacle_positions(map, pos, dir, rc, cc, test_obstacle_pos, found_loops) do
+    test_map = put_obstacle(map, test_obstacle_pos)
+
+    new_found_loops =
+      if loops?(test_map, pos, dir) do
+        [test_obstacle_pos | found_loops]
+      else
+        found_loops
+      end
+
+    looping_obstacle_positions(map, pos, dir, rc, cc, next_space(test_obstacle_pos, rc, cc), new_found_loops)
+  end
+
+  defp put_obstacle(map, obstacle_pos), do: Map.put(map, obstacle_pos, "#")
+
+  defp loops?(map, position, direction), do: loops?(map, position, direction, MapSet.new([{position, direction}]))
+
+  defp loops?(map, position, direction, visited) do
+    case walk(map, position, direction) do
+      {:continue, new_position, new_direction} ->
+        if MapSet.member?(visited, {new_position, new_direction}) do
+          true
+        else
+          loops?(map, new_position, new_direction, MapSet.put(visited, {position, direction}))
+        end
+
+      :exit ->
+        false
+    end
+  end
+
+  def part2(input) do
+    {map, num_rows, num_cols, guard_position, guard_direction} = parse_input(input)
+    looping_obstacle_positions(map, guard_position, guard_direction, num_rows, num_cols) |> length()
+  end
+
+  ## Part 1
+
   # IDEA
   # - Represent with same pos map technique
   # - Brute force solution: wishlist
@@ -31,12 +78,12 @@ defmodule Aoc2024.Day06 do
       |> List.flatten()
       |> Map.new()
 
-    line_length = (Enum.at(input_lines, 0) |> String.length()) + 1
+    text_line_length = (Enum.at(input_lines, 0) |> String.length()) + 1
     starting_point_index = :binary.match(input, "^") |> elem(0)
-    sp_row = div(starting_point_index, line_length)
-    sp_col = rem(starting_point_index, line_length)
+    sp_row = div(starting_point_index, text_line_length)
+    sp_col = rem(starting_point_index, text_line_length)
 
-    {map, {sp_row, sp_col}, 0}
+    {map, length(input_lines), text_line_length - 1, {sp_row, sp_col}, 0}
   end
 
   defp visited_positions(map, position, direction), do: visited_positions(map, position, direction, MapSet.new([position]))
@@ -66,7 +113,7 @@ defmodule Aoc2024.Day06 do
   defp turn(direction), do: rem(direction + 1, 4)
 
   def part1(input) do
-    {map, guard_position, guard_direction} = parse_input(input)
+    {map, _num_rows, _num_cols, guard_position, guard_direction} = parse_input(input)
     visited_positions(map, guard_position, guard_direction) |> MapSet.size()
   end
 
@@ -76,4 +123,8 @@ defmodule Aoc2024.Day06 do
   defp direction_to_pos_delta(3), do: {0, -1}
 
   defp pos_add({r1, c1}, {r2, c2}), do: {r1 + r2, c1 + c2}
+
+  defp next_space({r, c}, _row_count, col_count) when c < col_count - 1, do: {r, c + 1}
+  defp next_space({r, _c}, row_count, _col_count) when r < row_count - 1, do: {r + 1, 0}
+  defp next_space(_space, _row_count, _col_count), do: nil
 end
