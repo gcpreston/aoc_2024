19bb516393646128faf68d386db7a7b13ee53574
Graham Preston <graham.preston@gmail.com>
2025-01-20 01:51:23 +0100

    Progress on day 9 part 2
    Gives too high of an answer on real input, but test input works

--- lib/aoc2024/solutions/day_09.ex
+++ lib/aoc2024/solutions/day_09.ex
@@ -20,6 +20,7 @@ defmodule Aoc2024.Day09 do
       n = String.to_integer(c)
       is_file? = rem(i, 2) == 0
 
+      # ok wait how does this work LOL
       cond do
         n == 0 -> acc
         is_file? -> [{div(i, 2), n} | acc]
@@ -87,8 +88,102 @@ defmodule Aoc2024.Day09 do
   end
 
   def part1(input) do
-    data = parse_input(input)
+    data = parse_input(input) |> dbg()
     shifted = shift(data)
     checksum(Enum.reverse(shifted))
+  end
+
+  ## Part 2
+
+  defp parse_input_p2(input) do
+    # Want:
+    # - spaces front to back {index, size}
+    # - files back to front {index, size, id}
+
+    data = Enum.reverse(parse_input(input))
+
+    {files, _idx} =
+      Enum.reduce(data, {[], 0}, fn {file_id, count}, {acc, idx} ->
+        new_acc =
+          if is_nil(file_id) do
+            acc
+          else
+            [{idx, count, file_id} | acc]
+          end
+
+        {new_acc, idx + count}
+      end)
+
+    files = Enum.reverse(files)
+
+    {spaces, _idx} =
+      Enum.reduce(data, {[], 0}, fn {file_id, count}, {acc, idx} ->
+        new_acc =
+          if is_nil(file_id) do
+            [{idx, count} | acc]
+          else
+            acc
+          end
+
+        {new_acc, idx + count}
+      end)
+
+    spaces = Enum.reverse(spaces)
+
+    {files, spaces}
+  end
+
+  defp shift_p2(files, spaces) do
+    [first_file | rest_files] = files
+    shift_p2(Enum.reverse(rest_files), spaces, [first_file])
+  end
+
+  defp shift_p2([], _spaces, acc), do: acc
+
+  defp shift_p2([{_file_index, file_size, file_id} = last_file | rest_files] = files, spaces, acc) do
+    IO.puts("----- SHIFTING")
+    dbg(files)
+    dbg(spaces)
+    dbg(acc)
+
+    maybe_fitting_space =
+      Enum.with_index(spaces)
+      |> Enum.find(fn {{_input_idx, space_size}, _space_idx} ->
+        space_size >= file_size
+      end)
+
+    new_files = rest_files
+
+    {new_spaces, new_acc} =
+      case maybe_fitting_space do
+        {{raw_index, space_size}, space_index} ->
+          new_spaces =
+            if space_size == file_size do
+              List.delete_at(spaces, space_index)
+            else
+              List.replace_at(spaces, space_index, {raw_index + file_size, space_size - file_size})
+            end
+
+          {new_spaces, [{raw_index, file_size, file_id} | acc]}
+
+        nil ->
+          {spaces, [last_file | acc]}
+      end
+
+    shift_p2(new_files, new_spaces, new_acc)
+  end
+
+  defp checksum_p2(data) do
+    Enum.reduce(data, 0, fn {file_index, file_size, file_id}, total ->
+      file_checksum_part = file_id * Enum.sum(file_index..(file_index + file_size - 1))
+      total + file_checksum_part
+    end)
+  end
+
+  def part2(input) do
+    # answer too high
+    {files, spaces} = parse_input_p2(input)
+    shift_p2(files, spaces)
+    |> checksum_p2()
   end
 end
